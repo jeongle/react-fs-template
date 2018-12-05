8112babcf61cbd768b2f4e1b07c65213613cb7b7
jeongle <jyjjeong@gmail.com>
2018-12-04 23:27:51 -0800

    minor minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './client/index.jsx',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, '/public')
+    path: path.join(__dirname, '/public/dist')
   },
   module: {
     rules: [
